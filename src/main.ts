7fe2c875de47114fceddc22db6d6c07eb4a110d3
kwaichanz <alphezelphaz@gmail.com>
2023-07-22 17:27:30 +0700

    Update basic structure for the server. Added mongoose, dotenv.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(8000, () => console.log("server is running on port 8000"));
- 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const start = async () => {
+    console.log("Starting up...");
+  if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
+    console.log(process.env.MONGO_URI);
+    console.log("Connecting to MongoDB...");
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to connect to MongoDB");
+  }
+
+  app.listen(8000, () => console.log("server is running on port 8000"));
+};
+
+start();
